c3578c26c665bc59190dbf25ff5d3e0e5103e897
Fredrik Fossum <fredrik.fossum@finn.no>
2021-11-19 13:08:17 +0100

    floorplans

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,59 +1,39 @@
 <script context="module" lang="ts">
-	export const prerender = true;
+  export const prerender = true;
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+  let selectedFloor = "3";
 </script>
 
 <svelte:head>
-	<title>Home</title>
+  <title>Home</title>
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
+  <label for="floor-selector"> Floor </label>
+  <select id="floor-selector" bind:value={selectedFloor}>
+    <option value="3">3rd floor</option>
+    <option value="5">5th floor</option>
+  </select>
 
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+  {#if selectedFloor === "3"}
+    <img src="3rd-floor.png" alt="3rd floor floorplan" />
+  {:else if selectedFloor === "5"}
+    <img src="5th-floor.png" alt="4th floor floorplan" />
+  {/if}
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 1;
+  }
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  img {
+    filter: grayscale(1) opacity(0.5);
+  }
 </style>
